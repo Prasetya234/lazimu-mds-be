ea7bed0bf160558ab90394d4565d7fe5ffbc8aad
prasetya234 <putraprasetyadhany@gmail.com>
2021-08-26 10:49:59 +0700

    Lazimu rest api testing

---
+++ src/main/java/com/service/lazimu/ServiceLazimuApplication.java
@@ -0,0 +1,13 @@
+package com.service.lazimu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLazimuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceLazimuApplication.class, args);
+	}
+
+}
